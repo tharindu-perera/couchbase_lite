951a081d48f6472d46387d85a322c2828f76959c
tharindu <tharindu.perera@ge.com>
2020-07-24 10:52:18 +0530

    refactored and removed LoopHandler

--- android/src/main/java/com/saltechsystems/couchbase_lite/ReplicationConflictResolverListener.java
+++ android/src/main/java/com/saltechsystems/couchbase_lite/ReplicationConflictResolverListener.java
@@ -1,22 +1,15 @@
 package com.saltechsystems.couchbase_lite;
 
-import android.os.Handler;
-import android.os.Looper;
-
 import com.couchbase.lite.Conflict;
 import com.couchbase.lite.ConflictResolver;
-import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Document;
-import com.couchbase.lite.ListenerToken;
-import com.couchbase.lite.ReplicatorChange;
-import com.couchbase.lite.ReplicatorChangeListener;
 
 import io.flutter.plugin.common.EventChannel;
 
-public class ReplicationConflictResolverListener implements  EventChannel.StreamHandler, ConflictResolver {
-//    private CBManager mCBManager;
+public class ReplicationConflictResolverListener implements EventChannel.StreamHandler, ConflictResolver {
+    //    private CBManager mCBManager;
 //    private ListenerToken mListenerToken;
-    public  EventChannel.EventSink mEventSink;
+    public EventChannel.EventSink mEventSink;
 
     /*public ReplicationEventListener(CBManager manager) {
         mCBManager = manager;
@@ -26,7 +19,7 @@ public class ReplicationConflictResolverListener implements  EventChannel.Stream
      * IMPLEMENTATION OF EVENTCHANNEL.STREAMHANDLER
      */
 
-//    @Override
+    //    @Override
     public void onListen(Object o, final EventChannel.EventSink eventSink) {
         mEventSink = eventSink;
 //        mListenerToken = mCBManager.getReplicator().addChangeListener(this);
@@ -41,26 +34,15 @@ public class ReplicationConflictResolverListener implements  EventChannel.Stream
 //        mListenerToken = null;
         mEventSink = null;
     }
+
     @Override
     public Document resolve(final Conflict conflict) {
-//        System.out.println("inside  conlict reolver "+conflict.toString());
-        try{
-
-//            System.out.println("print mEventSink=="+mEventSink);
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-//                    System.out.println(">>>>>>>.inside  new Handler(Looper.getMainLooper())");
-                    mEventSink.success("conflict doc ID : "+conflict.getDocumentId());
-                }
-            });
-//            mEventSink.success("BUSYxeeeee");
-
-        }catch (Exception e){
-            System.err.println(">>>>> ERRROR : "+e.getMessage());
+        try {
+            mEventSink.success("conflict doc ID : " + conflict.getDocumentId());
+        } catch (Exception e) {
+            System.err.println(">>>>> ERROR : " + e.getMessage());
             e.printStackTrace();
         }
-
-        return conflict.getLocalDocument();
+        return conflict.getRemoteDocument();
     }
 }
